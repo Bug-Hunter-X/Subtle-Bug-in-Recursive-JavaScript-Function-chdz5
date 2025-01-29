7e4db92f841751d6045f9d31342f82a2664c696d
BugHunter-X <askzenai@gmail.com>
2025-01-29 09:18:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+function foo(a, b) {
+  if (a === 0) {
+    return 0; 
+  }
+  return b ? foo(a - 1, b - 1) : a;
+}
+
+console.log(foo(5, 2)); // Expected: 3, Actual: 3
+console.log(foo(5, 0)); // Expected: 5, Actual: 5
+console.log(foo(0, 5)); // Expected: 0, Actual: 0
+console.log(foo(5, 5)); // Expected: 0, Actual: 0
+console.log(foo(5, -1)); // Expected: Infinity, Actual: NaN
